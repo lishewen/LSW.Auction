22a63f0dc55773328b4dccba2cfffb9ab46cdd51
算神 <lishewen@gmail.com>
2016-07-12 09:03:17 +0800

    发布调整

--- 拍卖系统/src/拍卖系统/wwwroot/_references.js
+++ 拍卖系统/src/拍卖系统/wwwroot/_references.js
@@ -18,5 +18,6 @@
 /// <reference path="lib/jquery/dist/jquery.js" />
 /// <reference path="lib/jquery-validation/dist/jquery.validate.js" />
 /// <reference path="lib/jquery-validation-unobtrusive/jquery.validate.unobtrusive.js" />
+/// <reference path="lib/moment/moment.js" />
 /// <reference path="lib/signalr/jquery.signalR.js" />
 /// <reference path="lib/toastr/toastr.js" />
