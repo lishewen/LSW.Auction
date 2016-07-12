562b9fc95f8da8af58e4abeeb0be30c241e3a961
算神 <lishewen@gmail.com>
2016-07-12 09:38:40 +0800

    注册会员

--- 拍卖系统/src/拍卖系统/Controllers/WeixinController.cs
+++ 拍卖系统/src/拍卖系统/Controllers/WeixinController.cs
@@ -9,11 +9,17 @@ using LSW.Weixin.MP;
 using System.Text;
 using System.IO;
 using LSW.Weixin.MP.MvcExtension;
+using LSW.Weixin.MP.Entities.Menu;
+using LSW.Weixin.MP.AdvancedAPIs;
+using LSW.Weixin.MP.CommonAPIs;
+using 拍卖系统.Controllers.Attributes;
+using 拍卖系统.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace 拍卖系统.Controllers
 {
-    public class WeixinController : ControllerBase
-    {
+	public class WeixinController : ControllerBase
+	{
 		public WeixinController(ApplicationDbContext context) : base(context) { }
 		/// <summary>
 		/// 微信后台验证地址（使用Get），微信后台的“接口配置信息”的Url填写如：http://wx.car0774.com/weixin
@@ -57,6 +63,73 @@ namespace 拍卖系统.Controllers
 			//return Content(messageHandler.ResponseDocument.ToString());//v0.7-
 			//return new WeixinResult(messageHandler);//v0.8+ with MvcExtension
 			return new FixWeixinBugWeixinResult(messageHandler);//为了解决官方微信5.0以后软件换行bug暂时添加的方法，平时用上面一个方法即可
+		}
+		public IActionResult SetCustomMenu()
+		{
+			ButtonGroup bg = new ButtonGroup();
+
+			var subButton = new SubButton()
+			{
+				name = "会员中心"
+			};
+			subButton.sub_button.Add(new SingleViewButton()
+			{
+				url = OAuthApi.GetAuthorizeUrl(AppId, "http://lishewen.azurewebsites.net/Weixin/BindMemberCard", "car0774", OAuthScope.snsapi_base),
+				name = "拍卖中心"
+			});
+			subButton.sub_button.Add(new SingleViewButton()
+			{
+				url = "http://wifi.weixin.qq.com/mbl/connect.xhtml?type=1",
+				name = "微信连Wifi"
+			});
+			bg.button.Add(subButton);
+
+
+			bg.button.Add(new SingleViewButton()
+			{
+				name = "TJ首页",
+				url = "http://www.tjgemstone.com",
+			});
+
+			var result = CommonApi.CreateMenu(AccessToken, bg);
+
+			return Content(result.errmsg);
+		}
+		[WeixinAuth]
+		public async Task<IActionResult> BindMemberCard()
+		{
+			string id = OAuth.openid;
+
+			var member = await db.Members.SingleOrDefaultAsync(m => m.OpenId == id);
+			if (member != null)
+				return RedirectToAction("Index", "Member");
+
+			MemberBindModel model = new MemberBindModel();
+			model.OpenId = id;
+
+			return View(model);
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> BindMemberCard(MemberBindModel member)
+		{
+			//拉取会员信息
+			var info = UserApi.Info(AccessToken, OAuth.openid);
+
+			db.Members.Add(new Member
+			{
+				OpenId = member.OpenId,
+				Mobile = member.Phone,
+				Name = info.nickname,
+				NickName = info.nickname,
+				City = info.city,
+				Province = info.province,
+				Avatar = info.headimgurl
+			});
+			await db.SaveChangesAsync();
+
+			return RedirectToAction("Index", "Member");
 		}
 	}
 }
---
+++ 拍卖系统/src/拍卖系统/Models/MemberBindModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace 拍卖系统.Models
+{
+    public class MemberBindModel
+    {
+		[Display(Name = "手机号")]
+		[Required]
+		public string Phone { get; set; }
+		public string OpenId { get; set; }
+	}
+}
