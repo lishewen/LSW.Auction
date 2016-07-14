fba38d21017f185ee7321e7055c28bae60227599
算神 <lishewen@gmail.com>
2016-07-14 11:30:00 +0800

    api/Auctions

--- 拍卖系统/src/拍卖系统/Areas/Admin/Controllers/AuctionsController.cs
+++ 拍卖系统/src/拍卖系统/Areas/Admin/Controllers/AuctionsController.cs
@@ -10,138 +10,152 @@ using 拍卖系统.Models;
 
 namespace 拍卖系统.Areas.Admin.Controllers
 {
-    public class AuctionsController : ControllerBase
-    {
+	public class AuctionsController : ControllerBase
+	{
 		public AuctionsController(ApplicationDbContext context) : base(context) { }
 
-        // GET: Auctions
-        public async Task<IActionResult> Index()
-        {
-            return View(await db.Auctions.ToListAsync());
-        }
+		// GET: Auctions
+		public async Task<IActionResult> Index()
+		{
+			return View(await db.Auctions.ToListAsync());
+		}
 
-        // GET: Auctions/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+		public IActionResult Center()
+		{
+			return View();
+		}
 
-            var auction = await db.Auctions.SingleOrDefaultAsync(m => m.Id == id);
-            if (auction == null)
-            {
-                return NotFound();
-            }
+		// GET: Auctions/Details/5
+		public async Task<IActionResult> Details(int? id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
 
-            return View(auction);
-        }
+			var auction = await db.Auctions.SingleOrDefaultAsync(m => m.Id == id);
+			if (auction == null)
+			{
+				return NotFound();
+			}
 
-        // GET: Auctions/Create
-        public async Task<IActionResult> Create(int gid)
-        {
+			return View(auction);
+		}
+
+		// GET: Auctions/Create
+		public async Task<IActionResult> Create(int gid)
+		{
 			var good = await db.Goods.FirstOrDefaultAsync(g => g.Id == gid);
 			return View(new Auction { Gid = gid, Good = good, Name = good.Name });
-        }
+		}
 
-        // POST: Auctions/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create(Auction auction)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Add(auction);
-                await db.SaveChangesAsync();
-                return RedirectToAction("Index");
-            }
-            return View(auction);
-        }
+		// POST: Auctions/Create
+		// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+		// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Create(Auction auction)
+		{
+			if (ModelState.IsValid)
+			{
+				db.Add(auction);
+				await db.SaveChangesAsync();
+				return RedirectToAction("Index");
+			}
+			return View(auction);
+		}
 
-        // GET: Auctions/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+		// GET: Auctions/Edit/5
+		public async Task<IActionResult> Edit(int? id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
 
-            var auction = await db.Auctions.SingleOrDefaultAsync(m => m.Id == id);
-            if (auction == null)
-            {
-                return NotFound();
-            }
-            return View(auction);
-        }
+			var auction = await db.Auctions.SingleOrDefaultAsync(m => m.Id == id);
+			if (auction == null)
+			{
+				return NotFound();
+			}
+			return View(auction);
+		}
 
-        // POST: Auctions/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, Auction auction)
-        {
-            if (id != auction.Id)
-            {
-                return NotFound();
-            }
+		// POST: Auctions/Edit/5
+		// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+		// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Edit(int id, Auction auction)
+		{
+			if (id != auction.Id)
+			{
+				return NotFound();
+			}
 
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    db.Update(auction);
-                    await db.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!AuctionExists(auction.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction("Index");
-            }
-            return View(auction);
-        }
+			if (ModelState.IsValid)
+			{
+				try
+				{
+					db.Update(auction);
+					await db.SaveChangesAsync();
+				}
+				catch (DbUpdateConcurrencyException)
+				{
+					if (!AuctionExists(auction.Id))
+					{
+						return NotFound();
+					}
+					else
+					{
+						throw;
+					}
+				}
+				return RedirectToAction("Index");
+			}
+			return View(auction);
+		}
 
-        // GET: Auctions/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+		// GET: Auctions/Delete/5
+		public async Task<IActionResult> Delete(int? id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
 
-            var auction = await db.Auctions.SingleOrDefaultAsync(m => m.Id == id);
-            if (auction == null)
-            {
-                return NotFound();
-            }
+			var auction = await db.Auctions.SingleOrDefaultAsync(m => m.Id == id);
+			if (auction == null)
+			{
+				return NotFound();
+			}
 
-            return View(auction);
-        }
+			return View(auction);
+		}
 
-        // POST: Auctions/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var auction = await db.Auctions.SingleOrDefaultAsync(m => m.Id == id);
-            db.Auctions.Remove(auction);
-            await db.SaveChangesAsync();
-            return RedirectToAction("Index");
-        }
+		// POST: Auctions/Delete/5
+		[HttpPost, ActionName("Delete")]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> DeleteConfirmed(int id)
+		{
+			var auction = await db.Auctions.SingleOrDefaultAsync(m => m.Id == id);
+			db.Auctions.Remove(auction);
+			await db.SaveChangesAsync();
+			return RedirectToAction("Index");
+		}
+		[Produces("application/json")]
+		[Route("api/Auctions")]
+		[HttpGet]
+		public IEnumerable<Auction> GetAuctions()
+		{
+			return db.Auctions.Include(a => a.AuctionRecords)
+				.ThenInclude(a => a.Member)
+				.Where(a => a.StartTime <= DateTime.Now && a.EndTime >= DateTime.Now);
+		}
 
-        private bool AuctionExists(int id)
-        {
-            return db.Auctions.Any(e => e.Id == id);
-        }
-    }
+		private bool AuctionExists(int id)
+		{
+			return db.Auctions.Any(e => e.Id == id);
+		}
+	}
 }
